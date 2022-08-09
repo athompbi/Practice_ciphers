e7ed38f99f25ff34b42b8c00d5e174de0f8fa9a1
cloggerish <104546948+cloggerish@users.noreply.github.com>
2022-08-08 23:18:17 -0600

    Added decrypter

--- rail_fence_cipher.py
+++ rail_fence_cipher.py
@@ -1,40 +1,39 @@
-def three_rail_encode(string):
-    string_list = []
+### for decoding
+import math
+
+
+def encode(string):
+    """Runs text through the Rail Fence Cipher with 3 rails
     
+    Return:
+        encoded: encrypted string"""
+
+    ### make string iterable
+    string_list =list(string)
+    
+    ### lists to store rail values
     top_rail = []
     mid_rail = []
     bot_rail = []
 
-    for letter in string:
-        string_list.append(letter)
-
     length = len(string_list)
 
+    ### iterate through each letter by index
     for i in range(0,length):
+
+        ### if the index is odd, add to mid rail
         if (i % 2) == 1:
             mid_rail.append(string_list[i])
 
-        ## if odd index / 2 
+        ### if the index/2 is odd, add to bottom rail
         elif ((i/2) % 2) == 1:
             bot_rail.append(string_list[i])
         
+        ### else, add to the top rail
         else:
             top_rail.append(string_list[i])
 
-    # for letter in string_list:
-    #     index = string_list.index(letter)
-
-    #     ## if odd index
-    #     if (index % 2) == 1:
-    #         mid_rail.append(letter)
-
-    #     ## if odd index / 2 
-    #     elif (index/2 % 2) == 1:
-    #         bot_rail.append(letter)
-        
-    #     else:
-    #         top_rail.append(letter)
-
+    ### put rail lists into strings
     top_code = ''.join(top_rail)
     mid_code = ''.join(mid_rail)
     bot_code = ''.join(bot_rail)
@@ -44,29 +43,56 @@ def three_rail_encode(string):
     return encoded
 
 
-coded = three_rail_encode('WEAREDISCOVEREDFLEEATONCE')
+def decode(string):
+    """Runs coded string by Rail Fence cipher with 3 rails
+    through a decoder
+    
+    Return:
+        decoded: decrpted string"""
 
-print(coded)
+    length = len(string)
+    decoded_list = []
 
-if coded == 'WECRLTEERDSOEEFEAOCAIVDEN':
-    print('it worked!')
+    ### determine the length of the rails to find where breaks are
+    mid_len = math.floor(length / 2)
+    remain_len = length - mid_len
+    top_len = math.ceil(remain_len / 2)
+    bot_len = remain_len - top_len
 
-"""
-0 = top
-1 = mid
-2 = bot
-3 = mid
-4 = top
-5 = mid
-6 = bot
-7 = mid
-8 = top
+    ### first break is just top_len
+    sec_break = top_len + mid_len
 
-top = 0,4,8,12 (mult of 4?)
-mid = odd
-bot = 2,6,10,
+    ### break string into rails
+    top_rail = list(string[0:top_len])
+    mid_rail = list(string[top_len:sec_break])
+    bot_rail = list(string[sec_break:length])
 
-if index = odd -> mid
-if index/2 = even -> top
-else = bot
-"""
+    ### track how often the mid rail is used
+    mid_count = 0
+
+    ### add the indexed character from each rail to decoded_list
+    ### only add if the length of the section is greater than the index being called
+    for i in range(0,top_len):
+        decoded_list.append(top_rail[i])
+
+        if mid_len > mid_count:
+            decoded_list.append(mid_rail[mid_count])
+            mid_count += 1
+        else:
+            pass
+
+        if bot_len > i:
+            decoded_list.append(bot_rail[i])
+        else:
+            pass
+
+        if mid_len > mid_count: 
+            decoded_list.append(mid_rail[mid_count])
+            mid_count += 1
+        else:
+            pass
+        
+    ### put list back into string
+    decoded = ''.join(decoded_list)
+
+    return decoded
